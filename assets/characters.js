d349bb77f58b8a806d57fa44ad51b2509a6a9fab
modrakas <asmodrak@gmail.com>
2024-07-29 16:49:06 -0400

    Initial commit

---
+++ assets/characters.js
@@ -0,0 +1,45 @@
+const characters = (
+  {
+    name: "Ellie Williams",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/3/34/Part_II_Ellie_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215221019",
+    imgBack: "https://static.wikia.nocookie.net/thelastofus/images/7/73/Ellie_part_2_bow.png/revision/latest/scale-to-width-down/1000?cb=20200716014012",
+    age: "19 years",
+    woc: "Switchblade",
+    bio: "Ellie grew up as an orphan in Boston's quarantine zone and attended a FEDRA military school. At fourteen, she discovered she was immune to the Cordyceps infection. Marlene, leader of the Fireflies, believed Ellie was key to a vaccine and asked smuggler Joel Miller to escort her out of the QZ. Their simple task turned into a year-long journey across America, forming a close bond. In Salt Lake City, Joel learned Ellie had to die for a cure, so he killed the head surgeon and fled with Ellie to Jackson. For years, Ellie resented Joel for saving her. In 2038, Abby Anderson, the surgeon's daughter, killed Joel, leading Ellie to pursue Abby for revenge. Failing, Ellie retired to a farm with Dina and son JJ, but later abandoned them to hunt Abby again, ultimately sparing her. Ellie returned to the farm, accepting Joel's death and his choice to save her."
+  }, {
+    name: "Joel Miller",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/a/af/Part_II_Joel_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215221156",
+    imgBack: "https://static.wikia.nocookie.net/thelastofus/images/4/45/Joel_guitar1.jpg/revision/latest/scale-to-width-down/1000?cb=20200715195557",
+    age: "56 years",
+    woc: "Revolver",
+    bio: "Joel Miller is the main character in The Last of Us Part I and appears in The Last of Us: Left Behind and The Last of Us Part II. A survivor in post-apocalyptic America, Joel lost his daughter Sarah early in the Cordyceps outbreak and became a ruthless smuggler. Tasked with protecting Ellie Williams, the key to humanity's survival, Joel formed a strong bond with her."
+  }, {
+    name: "Abby Anderson",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/f/fd/Part_II_Abby_2035_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215213426",
+    imgBack: "https://static.wikia.nocookie.net/thelastofus/images/1/16/AbbyPart2.png/revision/latest?cb=20200621165601",
+    age: "20-21 years",
+    woc: "Military Pistol",
+    bio: "Abigail 'Abby' Anderson is the dual protagonist of The Last of Us Part II and a playable character in No Return mode. After Joel Miller killed her father, Firefly member Jerry Anderson, Abby sought revenge. Following her vengeance, she found new purpose while journeying with her companion Lev."
+  }, {
+    name: "Dina",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/f/f4/Part_II_Dina_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215214241",
+    imgBack: "https://preview.redd.it/icisfbj1pj661.jpg?width=1080&crop=smart&auto=webp&s=11923243a2872ff82c7f79fea944dd0173db04d5",
+    age: "20-23 years",
+    woc: "Jacksonian Bolt-action Rifle",
+    bio: "Dina is the deuteragonist of Ellie's story in The Last of Us Part II and a playable character in the No Return mode.She is Ellie's girlfriend and helps Ellie in her quest for revenge."
+  }, {
+    name: "Tommy Miller",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/f/f0/Part_II_Tommy_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230216035810",
+    imgBack: "https://static.wikia.nocookie.net/thelastofus/images/a/a7/Tommy_Seattle_Profile.png/revision/latest?cb=20200625064807",
+    age: "50-51 years",
+    woc: "Scoped Rifle",
+    bio: "Tommy Miller, a supporting character in The Last of Us Part I and II and playable in No Return mode, is Joel's younger brother. Compassionate and caring, Tommy survived the outbreak's early years with Joel but later sought a more meaningful life. He joined the Fireflies and eventually found redemption with his wife Maria and the Jackson community. When Abby kills Joel, Tommy seeks out revenge with Ellie. Tommy ends up badly injured, unable to avenge his brother's death, and tells Ellie she must be the one to seek revenge."
+  }, {
+    name: "Lev Benioff",
+    imgMain: "https://static.wikia.nocookie.net/thelastofus/images/a/a3/Part_II_Lev_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215214140",
+    imgBack: "(Lev w/ bow) https://gaymingmag.com/wp-content/uploads/2020/07/levtlou2-1024x576.jpg",
+    age: "13 years",
+    woc: "Bow and Arrow",
+    bio: "Lev is a former member of the Seraphites who is forced into exile after his community rejects him for being transgender. He is a playable character in No Return mode and the deuteragonist in Abby Anderson's story. Lev forms a bond with Abby, a member of the Washington Liberation Front (WLF) and helps Abby see a new purpose to life."
+  }
+);
