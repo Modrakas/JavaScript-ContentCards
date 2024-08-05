c92320b7ea13ff6d6348c72edfd5473bed935137
modrakas <asmodrak@gmail.com>
2024-08-05 08:49:23 -0400

    fixed typo in characters.js

--- assets/characters.js
+++ assets/characters.js
@@ -6,7 +6,7 @@ export const characters = [
     weapon: 'Switchblade',
     residence: 'Jackson, WY',
     hobby: 'Playing guitar, journaling, and drawing.',
-    bio: "Ellie, an orphan from Boston's QZ, learned she was immune to the Cordyceps infection. Marlene, leader of the Fireflies, believed Ellie was the key to a vaccine and asked Joel to escort her out of the QZ. During their journey the two formed a close bond. When  Joel learned Ellie had to die for a cure, he killed the head surgeon and fled with Ellie to Jackson. Ellie later resented Joel for saving her, and in 2038, Abby, the surgeon's daughter, killed Joel. Ellie pursued Abby for revenge but ultimately spared her, returning to a farm only to leave again. In the end, Ellie accepted Joel's death and his choice to save her."
+    bio: "Ellie, an orphan from Boston's QZ, learned she was immune to the Cordyceps infection. Marlene, leader of the Fireflies, believed Ellie was the key to a vaccine and asked Joel to escort her out of the QZ. During their journey the two formed a close bond. When  Joel learned Ellie had to die for a cure, he killed the head surgeon and fled with Ellie to Jackson. Ellie later resented Joel for saving her. In 2038, Abby, the surgeon's daughter, Abby, killed Joel. Ellie pursued Abby for revenge but ultimately spared her, returning to a farm only to leave again. In the end, Ellie accepted Joel's death and his choice to save her."
   },
   {
     name: "Joel Miller",
@@ -22,7 +22,7 @@ export const characters = [
     image: "assets/images/abby.png",
     age: "20-21 years",
     weapon: "Military Pistol",
-    residence: 'Seattel, WA',
+    residence: 'Seattle, WA',
     hobby: 'Petting giraffes, training and killing Joel.',
     bio: "Abigail 'Abby' Anderson is the dual protagonist of The Last of Us Part II and a playable character in No Return mode. After Joel Miller killed her father, Firefly member Jerry Anderson, Abby sought revenge. Following her vengeance, she found new purpose while journeying with her companion Lev."
   }, 
@@ -49,7 +49,7 @@ export const characters = [
     image: "assets/images/lev.png",
     age: "13 years",
     weapon: "Bow and Arrow",
-    residence: 'Seraphire Island, Wa',
+    residence: 'Seraphite Island, WA',
     hobby: 'Tightrope walking, shooting arrows, and helping Abby.',
     bio: "Lev is a former member of the Seraphites who is forced into exile after his community rejects him for being transgender. He is a playable character in No Return mode and the deuteragonist in Abby Anderson's story. Lev forms a bond with Abby, a member of the Washington Liberation Front (WLF) and helps Abby see a new purpose to life."
   },
